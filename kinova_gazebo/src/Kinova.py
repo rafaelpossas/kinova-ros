e198a5f454183fe6eb8be2e9b609481b4a9c7dc1
Rafael Possas <rafaelpossas@gmail.com>
2018-09-18 13:34:47 +1000

    New Script

--- kinova_gazebo/src/Kinova.py
+++ kinova_gazebo/src/Kinova.py
@@ -10,6 +10,7 @@ from gazebo_msgs.srv import (
 )
 from geometry_msgs.msg import (
     PoseStamped,
+    PointStamped,
     Pose,
     Point,
     Quaternion,
@@ -27,14 +28,15 @@ class KinovaUtilities(object):
 
         ## Instantiate a `RobotCommander`_ object. This object is the outer-level interface to
         ## the robot:
-        robot = moveit_commander.RobotCommander()
+        self.robot = moveit_commander.RobotCommander()
 
-        group_names = robot.get_group_names()
-        print "============ Robot Groups:", robot.get_group_names()
+        group_names = self.robot.get_group_names()
+        print "============ Robot Groups:", self.robot.get_group_names()
 
         ## Instantiate a `PlanningSceneInterface`_ object.  This object is an interface
         ## to the world surrounding the robot:
-        scene = moveit_commander.PlanningSceneInterface()
+        self.scene = moveit_commander.PlanningSceneInterface()
+        self.box_name = "cube"
 
         ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
         ## to one group of joints.  In this case the group is the joints in the Panda
@@ -55,7 +57,7 @@ class KinovaUtilities(object):
         # Sometimes for debugging it is useful to print the entire state of the
         # robot:
         print "============ Printing robot state"
-        print robot.get_current_state()
+        print self.robot.get_current_state()
         print ""
 
         current_pose = self.arm_group.get_current_pose()
@@ -72,12 +74,13 @@ class KinovaUtilities(object):
         rate = rospy.Rate(10.0)
 
 
-        self.tf.waitForTransform("/world", "/tag_0", rospy.Time(), rospy.Duration(4.0))
+        self.tf.waitForTransform("/world", "/tag_0", rospy.Time(), rospy.Duration(4))
         now = rospy.Time(0)
         (trans, rot) = self.tf.lookupTransform("/world", "/tag_0", now)
 
         print trans
 
+        self.add_object("cube", trans,rot)
 
         # tag_pos = geometry_msgs.msg.PoseStamped()
         # tag_pos.header.frame_id = "tag_0"
@@ -88,26 +91,64 @@ class KinovaUtilities(object):
         # rospy.loginfo(start_pos_manual)
         #
         # self.return_home_position()
-        # self.pre_grasp_y_to_origin(trans)
-        #
-        # self.return_home_position()
-        # self.pre_grasp_y_from_origin(trans)
-        #
-        # self.return_home_position()
-        # self.pre_grasp_x_to_origin(trans)
+        # self.pre_grasp_face_4(trans)
 
         # self.return_home_position()
+        # self.pre_grasp_face_2(trans)
+        # # #
+        self.return_home_position()
+        self.pre_grasp_face_1(trans)
 
-        self.add_object("cube", trans)
+        # self.return_home_position()
+
+    def wait_for_state_update(self, box_is_known=False, box_is_attached=False, timeout=4):
+        # Copy class variables to local variables to make the web tutorials more clear.
+        # In practice, you should use the class variables directly unless you have a good
+        # reason not to.
+        box_name = self.box_name
+        scene = self.scene
+
+        ## BEGIN_SUB_TUTORIAL wait_for_scene_update
+        ##
+        ## Ensuring Collision Updates Are Receieved
+        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        ## If the Python node dies before publishing a collision object update message, the message
+        ## could get lost and the box will not appear. To ensure that the updates are
+        ## made, we wait until we see the changes reflected in the
+        ## ``get_known_object_names()`` and ``get_known_object_names()`` lists.
+        ## For the purpose of this tutorial, we call this function after adding,
+        ## removing, attaching or detaching an object in the planning scene. We then wait
+        ## until the updates have been made or ``timeout`` seconds have passed
+        start = rospy.get_time()
+        seconds = rospy.get_time()
+        while (seconds - start < timeout) and not rospy.is_shutdown():
+            # Test if the box is in attached objects
+            attached_objects = scene.get_attached_objects([box_name])
+            is_attached = len(attached_objects.keys()) > 0
+
+            # Test if the box is in the scene.
+            # Note that attaching the box will remove it from known_objects
+            is_known = box_name in scene.get_known_object_names()
+
+            # Test if we are in the expected state
+            if (box_is_attached == is_attached) and (box_is_known == is_known):
+                return True
+
+            # Sleep so that we give other threads time on the processor
+            rospy.sleep(0.1)
+            seconds = rospy.get_time()
+
+        # If we exited the while loop without returning then we timed out
+
+        return False
 
 
+    def add_object(self, name, pos, rot):
+        timeout = 4
 
-    def add_object(self, name, pos):
-        scene = moveit_commander.PlanningSceneInterface()
-        robot = moveit_commander.RobotCommander()
-        scene.remove_world_object(name)
+        self.scene.remove_world_object(name)
         p = PoseStamped()
-        p.header.frame_id = robot.get_planning_frame()
+        p.header.frame_id = self.robot.get_planning_frame()
 
         # p.pose.position.x = 0.917182
         # p.pose.position.y = 0.858523
@@ -117,9 +158,14 @@ class KinovaUtilities(object):
         p.pose.position.y = pos[1] - 0.03
         p.pose.position.z = pos[2] - 0.02
 
-        scene.add_box(name, p, (0.05,0.05,0.05))
+        p.pose.orientation.x = rot[0]
+        p.pose.orientation.y = rot[1]
+        p.pose.orientation.z = rot[2]
+        p.pose.orientation.w = rot[3]
 
-        rospy.sleep(1)
+        self.scene.add_box(name, p, (0.05,0.05,0.05))
+
+        return self.wait_for_state_update(box_is_known=True, timeout=timeout)
 
     ## END_SUB_TUTORIAL
     def return_home_position(self):
@@ -155,8 +201,19 @@ class KinovaUtilities(object):
     #     plan = self.gripper_group.plan()
     #     self.gripper_group.execute(plan)
     #     rospy.sleep(1)
-    def pre_grasp_y_from_origin(self, trans):
+    def pre_grasp_face_2(self, trans):
         self.close_gripper()
+
+        self.tf.waitForTransform("/world", "/tag_0", rospy.Time(0), rospy.Duration(4))
+
+        cube_point = PointStamped()
+        cube_point.header.frame_id = "tag_0"
+        cube_point.header.stamp = rospy.Time(0)
+        cube_point.point.x = -0.01
+        cube_point.point.y = -0.1
+        cube_point.point.z = 0.0
+
+        trans = self.tf.transformPoint("world", cube_point)
 
         pose_goal = geometry_msgs.msg.Pose()
 
@@ -166,17 +223,28 @@ class KinovaUtilities(object):
         pose_goal.orientation.z = 0.0204806930057
         pose_goal.orientation.w = 0.00745991303797
 
-        pose_goal.position.x = trans[0] + 0.017
-        pose_goal.position.y = trans[1] - 0.08
-        pose_goal.position.z = trans[2] - 0.04
+        pose_goal.position.x = trans.point.x
+        pose_goal.position.y = trans.point.y
+        pose_goal.position.z = trans.point.z
 
         self.arm_group.set_pose_target(pose_goal)
         self.arm_group.go(wait=True)
         self.arm_group.stop()
         self.arm_group.clear_pose_targets()
 
-    def pre_grasp_y_to_origin(self, trans):
+    def pre_grasp_face_4(self, trans):
         self.close_gripper()
+
+        self.tf.waitForTransform("/world", "/tag_0", rospy.Time(0), rospy.Duration(4))
+
+        cube_point = PointStamped()
+        cube_point.header.frame_id = "tag_0"
+        cube_point.header.stamp = rospy.Time(0)
+        cube_point.point.x = -0.02
+        cube_point.point.y = 0.07
+        cube_point.point.z = 0.0
+
+        trans = self.tf.transformPoint("world", cube_point)
 
         pose_goal = geometry_msgs.msg.Pose()
 
@@ -186,17 +254,28 @@ class KinovaUtilities(object):
         pose_goal.orientation.z = -0.00329246532167
         pose_goal.orientation.w = 0.0159553576136
 
-        pose_goal.position.x = trans[0] + 0.017
-        pose_goal.position.y = trans[1] + 0.07
-        pose_goal.position.z = trans[2] - 0.04
+        pose_goal.position.x = trans.point.x
+        pose_goal.position.y = trans.point.y
+        pose_goal.position.z = trans.point.z
 
         self.arm_group.set_pose_target(pose_goal)
         self.arm_group.go(wait=True)
         self.arm_group.stop()
         self.arm_group.clear_pose_targets()
 
-    def pre_grasp_x_to_origin(self, trans):
+    def pre_grasp_face_1(self, trans):
         self.close_gripper()
+
+        self.tf.waitForTransform("/world", "/tag_0", rospy.Time(0), rospy.Duration(4))
+
+        cube_point = PointStamped()
+        cube_point.header.frame_id = "tag_0"
+        cube_point.header.stamp = rospy.Time(0)
+        cube_point.point.x = 0.07
+        cube_point.point.y = -0.01
+        cube_point.point.z = 0.0
+
+        trans = self.tf.transformPoint("world", cube_point)
 
         pose_goal = geometry_msgs.msg.Pose()
 
@@ -206,9 +285,9 @@ class KinovaUtilities(object):
         pose_goal.orientation.z = -0.0335475179298
         pose_goal.orientation.w = 0.0444419357171
 
-        pose_goal.position.x = trans[0] + 0.09
-        pose_goal.position.y = trans[1]
-        pose_goal.position.z = trans[2] - 0.04
+        pose_goal.position.x = trans.point.x
+        pose_goal.position.y = trans.point.y
+        pose_goal.position.z = trans.point.z
 
         self.arm_group.set_pose_target(pose_goal)
         self.arm_group.go(wait=True)
@@ -218,9 +297,9 @@ class KinovaUtilities(object):
 
     def close_gripper(self):
         joint_goal = self.gripper_group.get_current_joint_values()
-        joint_goal[0] = 1.08
-        joint_goal[1] = 1.08
-        joint_goal[2] = 1.08
+        joint_goal[0] = 1.2
+        joint_goal[1] = 1.2
+        joint_goal[2] = 1.2
 
         # The go command can be called with joint values, poses, or without any
         # parameters if you have already set the pose or joint target for the group
